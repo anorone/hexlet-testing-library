9f8437117c153c3b198042648c121f73ea834d56
zhenia-chugaev <zhenia.chugaev@bk.ru>
2025-03-29 14:08:52 +0100

    add test case

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,9 +1,29 @@
 // @ts-check
 
-import { test, expect } from 'vitest';
+import { test, expect, beforeEach, afterEach } from 'vitest';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 
-test('main', async () => {
-  document.body.innerHTML = '<h1>Hexlet</h1>';
+import { TicTacToe } from '@hexlet/tic-tac-toe';
 
-  expect(document.body).toHaveTextContent('Hexlet');
+let user;
+
+beforeEach(() => {
+  const game = new TicTacToe(document.body);
+  game.start();
+  user = userEvent.setup();
+});
+
+afterEach(() => {
+  document.body.innerHTML = '';
+})
+
+test('tic-tac-toe', async () => {
+  const input1 = screen.getByLabelText('Player 1');
+  const input2 = screen.getByLabelText('Player 2');
+  await user.type(input1, 'Joe');
+  await user.type(input2, 'Bo');
+  const submitButton = screen.getByText('Start Game');
+  await user.click(submitButton);
+  expect(document.body).toHaveTextContent('Joe, you are up!');
 });
--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,2 @@
 import '@testing-library/jest-dom/vitest';
+import "@hexlet/tic-tac-toe/public/style.css";
